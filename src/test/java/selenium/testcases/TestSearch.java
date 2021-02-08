d0fde7e2727929fd7d5e9e5db34c15b9f66ac160
Prachiti-06 <67158460+Prachiti-06@users.noreply.github.com>
2021-02-08 14:10:50 +0100

    Update TestSearch.java

--- src/test/java/selenium/testcases/TestSearch.java
+++ src/test/java/selenium/testcases/TestSearch.java
@@ -10,18 +10,18 @@ public class TestSearch {
 
         System.out.println("start Testing...");
         testCases.setup();
-       // testCases.tc_Search_1();
-       /* testCases.tc_Search_2();
+        testCases.tc_Search_1();
+        testCases.tc_Search_2();
         testCases.tc_Search_3();
         testCases.tc_Search_4();
         testCases.tc_Search_5();
-        testCases.tc_Search_6();*/
+        testCases.tc_Search_6();
         testCases.tc_Search_7();
-       /* testCases.tc_Search_8();
+        testCases.tc_Search_8();
         testCases.tc_Search_9();
         testCases.tc_Search_10();
         testCases.tc_Search_11();
-        testCases.tc_Search_12();*/
+        testCases.tc_Search_12();
         testCases.afterClass();
 
     }
